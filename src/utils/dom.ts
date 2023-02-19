26ba0b4e16004bf0739040f9e4f4c411e5351948
yunho <kimyouknow@naver.com>
2023-02-19 21:41:06 +0900

    Feat: dom 생성함수에 combineNodes 추가(map 사용 가능)

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,42 +1,50 @@
-type Arg = string | number | HTMLElement | DocumentFragment
+export type BaseNode = DocumentFragment | Document | Element | HTMLElement
 
-export const dom = (template: TemplateStringsArray, ...args: Arg[]): HTMLElement => {
-  const container = document.createElement('template')
+type Arg = Array<BaseNode> | BaseNode | string | number
 
-  const replaceTypeToString = (arg: Arg) => {
-    if (typeof arg === 'string') {
-      return arg
-    } else if (typeof arg === 'number') {
-      return `${arg}`
-    }
-    return ''
+export const convertNodeToString = (element: Node): string => {
+  return element.nodeValue || ''
+}
+
+export const convertDFToString = (fragment: BaseNode): string => {
+  const container = document.createElement('div')
+  container.appendChild(fragment.cloneNode(true))
+  return container.innerHTML
+}
+
+const replaceTypeToString = (arg: Arg) => {
+  if (typeof arg === 'string') {
+    return arg
+  } else if (typeof arg === 'number') {
+    return `${arg}`
+  } else if (arg instanceof DocumentFragment) {
+    return convertDFToString(arg)
+  } else if (arg instanceof Node) {
+    return convertNodeToString(arg)
   }
+  return ''
+}
 
-  const serializeToString = (node: Node): string => {
-    const serializer = new XMLSerializer()
-    return serializer.serializeToString(node)
-  }
-
-  const convertToString = (fragment: DocumentFragment): string => {
-    const container = document.createElement('div')
-    container.appendChild(fragment.cloneNode(true))
-    return container.innerHTML
-  }
-
-  const nodes = args.map(arg => {
-    if (arg instanceof DocumentFragment) {
-      return convertToString(arg)
-    } else if (arg instanceof Node) {
-      return serializeToString(arg)
+const combineNodes = (nodes: Array<BaseNode>): string => {
+  const fragment = document.createDocumentFragment()
+  nodes.forEach(node => {
+    if (node instanceof DocumentFragment) {
+      fragment.appendChild(node)
     } else {
-      return replaceTypeToString(arg)
+      const container = document.createElement('div')
+      container.innerHTML = replaceTypeToString(node)
+      fragment.appendChild(container.firstChild as Node)
     }
   })
+  return convertDFToString(fragment)
+}
+
+export const dom = (template: TemplateStringsArray, ...args: Arg[]): HTMLElement => {
+  const nodes = args.map(arg => (arg instanceof Array ? combineNodes(arg) : replaceTypeToString(arg)))
+  const container = document.createElement('template')
   container.innerHTML = String.raw(template, ...nodes)
   return container.content.cloneNode(true) as HTMLElement
 }
-
-export type BaseNode = DocumentFragment | Document | Element | HTMLElement
 
 export const $ = <T extends Element>(selector: string, base: BaseNode = document) => base.querySelector(selector) as T
 
