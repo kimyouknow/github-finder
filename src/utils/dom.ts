a238fb1108c2ec87a1b69a85138e229f69c08c0c
yunho <kimyouknow@naver.com>
2023-03-06 16:53:07 +0900

    Refactor: htmlDom, html 유틸 함수
    - convertArgToString
    - combineArgToString

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -2,56 +2,20 @@ export type BaseNode = DocumentFragment | Document | Element | HTMLElement;
 
 type Arg = Array<BaseNode | string> | BaseNode | string | number;
 
-export const convertNodeToString = (element: Node): string => {
-  return element.nodeValue || '';
-};
+const convertArgToString = (arg: Arg) => (arg instanceof HTMLElement ? arg.outerHTML : arg);
 
-export const convertDFToString = (fragment: BaseNode): string => {
-  const container = document.createElement('div');
-  container.appendChild(fragment.cloneNode(true));
-  return container.innerHTML;
-};
-
-const replaceTypeToString = (arg: Arg) => {
-  if (typeof arg === 'string') {
-    return arg;
-  } else if (typeof arg === 'number') {
-    return `${arg}`;
-  } else if (arg instanceof DocumentFragment) {
-    return convertDFToString(arg);
-  } else if (arg instanceof Node) {
-    return convertNodeToString(arg);
-  }
-  return '';
-};
-
-const combineNodes = (nodes: Array<BaseNode | string>): string => {
-  const fragment = document.createDocumentFragment();
-  nodes.forEach(node => {
-    if (node instanceof DocumentFragment) {
-      fragment.appendChild(node);
-    } else {
-      const container = document.createElement('div');
-      container.innerHTML = replaceTypeToString(node);
-      fragment.appendChild(container.firstChild as Node);
-    }
-  });
-  return convertDFToString(fragment);
-};
+const combineArgToString = (arg: Arg) =>
+  arg instanceof Array ? arg.map(convertArgToString).join('') : convertArgToString(arg);
 
 export const htmlDom = (template: TemplateStringsArray, ...args: Arg[]): HTMLElement => {
-  const nodes = args.map(arg =>
-    arg instanceof Array ? combineNodes(arg) : replaceTypeToString(arg),
-  );
+  const nodes = args.map(combineArgToString);
   const container = document.createElement('template');
   container.innerHTML = String.raw(template, ...nodes);
-  return container.content.firstElementChild?.cloneNode(true) as HTMLElement;
+  return container.content.firstChild?.cloneNode(true) as HTMLElement;
 };
 
 export const html = (template: TemplateStringsArray, ...args: Arg[]): string => {
-  const nodes = args.map(arg =>
-    arg instanceof Array ? combineNodes(arg) : replaceTypeToString(arg),
-  );
+  const nodes = args.map(combineArgToString);
   return String.raw(template, ...nodes);
 };
 
