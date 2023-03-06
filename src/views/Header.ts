6464ad0f426a4bf13c6a94b4cdba60aca573c6d6
yunho <kimyouknow@naver.com>
2023-03-06 18:07:28 +0900

    Design: header, form

---
+++ src/views/Header.ts
@@ -0,0 +1,11 @@
+import { html } from '@/utils/dom';
+
+const Header = () => {
+  return html`
+    <header class="header">
+      <h1>Github Finder</h1>
+    </header>
+  `;
+};
+
+export default Header;
