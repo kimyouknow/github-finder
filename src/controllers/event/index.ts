40913a5b7c1e945fe20301dd4f645c637b23f130
yunho <kimyouknow@naver.com>
2023-03-13 17:00:55 +0900

    Feat: main로고 클릭시 메인 페이지로 이동

--- src/controllers/event/index.ts
+++ src/controllers/event/index.ts
@@ -1,7 +1,9 @@
+import handleLayoutEvent from '@/controllers/event/layout.event';
 import handleSearchForm from '@/controllers/event/searchForm.event';
 
 const addEventHandler = () => {
   handleSearchForm();
+  handleLayoutEvent();
 };
 
 export default addEventHandler;
